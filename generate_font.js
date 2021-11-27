b6387e737989eae125505de467e838df9eebcdc2
aziz <Aziz.954856>
2021-11-27 23:32:46 +0300

    fixed svg stroke to path

--- generate_font.js
+++ generate_font.js
@@ -14,17 +14,27 @@ fs.readdir('yartu/icons', function(err, items) {
     fontName: 'yartu-icons',
     
     // https://github.com/nfroidure/svgicons2svgfont options
-    normalize: false,
+    normalize: true,
     html:true,
+    htmlDest: 'yartu/generated_fonts/index.html',
     htmlTemplate: 'yartu/templates/html.hbs',
 
     cssTemplate: 'yartu/templates/font-css.hbs',
     templateOptions: {
       classPrefix: '',
-      baseSelector: '.yartu-icon'
+      baseSelector: '.yi'
     },
     types: ['svg', 'ttf', 'woff', 'eot'],
     startCodepoint: 0xF101,
+    formatOptions: {
+      // options to pass specifically to the svgicons2svgfont generator
+      svg: {
+          centerHorizontally:true,
+          centerVertically:true,
+          normalize:true,
+          fontHeight: 1000,//this option scale icons to max font size
+      }
+  }
   }, function (error) {
     if (error) {
       console.log('Fail!', error);
